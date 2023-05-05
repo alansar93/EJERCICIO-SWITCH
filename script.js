3ddcec26bdf44b4916a0fa9068ac6f921c42e6b0
Alan  Sarchman <alansarhchman@live.com.argit config --global user.emailgit config --globalgit config --global config user.namegit initgit config --global user.email alansarhchman@live.com.ar>
2023-05-05 20:45:10 -0300

    hasta el punto 3

--- script.js
+++ script.js
@@ -3,25 +3,29 @@ const TIPO_COLOR = document.querySelector("select");
 const PARRAFO = document.querySelector("#parrafo");
 
 
-
 function cambioColor() {
+   
     switch (TIPO_COLOR.value) {
         case "color 1":
             PAGINA.style.color = "white";
             PAGINA.style.backgroundColor = "black";
+            TIPO_COLOR.style.color = "white";
             break;
         case "color 2":
             PAGINA.style.color = "violet";
             PAGINA.style.backgroundColor = "brown";
+            TIPO_COLOR.style.color = "violet";
             break;
         case "color 3":
             PAGINA.style.color = "pink";
             PAGINA.style.backgroundColor = "grey";
+            TIPO_COLOR.style.color = "pink";
             break
         default:
             PARRAFO.innerHTML = "error"
             PAGINA.style.backgroundColor = "white"
             PAGINA.style.color = "black"
-           
+            TIPO_COLOR.style.color = "red";
+
     }
 }
